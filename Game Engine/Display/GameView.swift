4011b6980155722e988e5099d89c7e81ed3b50e7
Timothy Wilson <timothywilson@protonmail.com>
2022-10-23 15:06:56 -0400

    lesson 7

--- Game Engine/Display/GameView.swift
+++ Game Engine/Display/GameView.swift
@@ -2,10 +2,7 @@ import MetalKit
 
 class GameView: MTKView {
     
-    var verticies: [Vertex]!
-    var vertexBuffer: MTLBuffer!
-    
-    
+    var renderer: Renderer!
     required init(coder: NSCoder) {
         super.init(coder: coder)
         
@@ -17,34 +14,11 @@ class GameView: MTKView {
         
         self.colorPixelFormat = Preferences.MainPixelFormat // most common for applications
         
-        createVerticies()
-        createBuffers()
-    }
-    
-    func createVerticies() {
-        verticies = [
-            Vertex(position: SIMD3<Float>( 0, 1, 0), color: SIMD4<Float>(1,0,0,1)),
-            Vertex(position: SIMD3<Float>(-1,-1, 0), color: SIMD4<Float>(0,1,0,1)),
-            Vertex(position: SIMD3<Float>( 1,-1, 0), color: SIMD4<Float>(0,0,1,1)),
-        ]
-    }
-    
-    func createBuffers() {
-        vertexBuffer = Engine.Device.makeBuffer(bytes: verticies, length: Vertex.stride(verticies.count) * verticies.count, options: [])
-    }
-    
-    override func draw(_ dirtyRect: NSRect) {
-        guard let drawable = self.currentDrawable, let renderPassDescriptor = self.currentRenderPassDescriptor else { return }
+        self.renderer = Renderer()
         
-        let commandBuffer = Engine.CommandQueue.makeCommandBuffer()
-        let renderCommandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
-        renderCommandEncoder?.setRenderPipelineState(RenderPipelineStateLibrary.PipelineState(.Basic))
+        self.delegate = renderer
         
-        renderCommandEncoder?.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
-        renderCommandEncoder?.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: verticies.count)
-        
-        renderCommandEncoder?.endEncoding()
-        commandBuffer?.present(drawable)
-        commandBuffer?.commit()
+        // M&K Input
+
     }
 }
