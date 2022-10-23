ab876941be8d70e153625ddcbddfb293cd9351fc
Timothy Wilson <timothywilson@protonmail.com>
2022-10-23 10:55:01 -0400

    Initial Commit

---
+++ Game Engine/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Game Engine
+//
+//  Created by Timothy Wilson on 10/23/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
